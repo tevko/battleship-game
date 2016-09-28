a322a777af787c86dc2bf874178d2b84d2deecac
Tim <timevko@gmail.com>
2016-09-27 23:11:18 -0400

    add coord creation algorithm

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
+const apiController = '/controllers/ap/');
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+//API Routing
+
+//create new game
+app.put('/api/newgame/', require(`/${apiController}/createGame`));
+
+app.delete('/api/destroygame/', (req, res, next) => {
+	//delete game
+});
+
+app.post('/api/playermove', (req, res, next) => {
+	//human updates board
+});
+
+app.get('/api/cpumove', (req, res, next) => {
+	//robot updates board
 });
 
 app.listen(3000);
--- controllers/api/createGame.js
+++ controllers/api/createGame.js
@@ -10,36 +10,17 @@ const createGame = (req, res, next) => {
 	***/
 	const userCoords = req.body.userGameCoords;
 	const vessels = ['carrier', 'battleship', 'jetski', 'armed octopus', 'squirtgun'];
-	const availableCoords = [
-		[1,1],
-		[1,2],
-		[1,3],
-		[1,4],
-		[1,5],
-		[2,1],
-		[2,2],
-		[2,3],
-		[2,4],
-		[2,5],
-		[3,1],
-		[3,2],
-		[3,3],
-		[3,4],
-		[3,5],
-		[4,1],
-		[4,2],
-		[4,3],
-		[4,4],
-		[4,5],
-		[5,1],
-		[5,2],
-		[5,3],
-		[5,4],
-		[5,5]
-	];
-	const getAvailableCoords = availableCoords.forEach((cordMap, idx, arr) => {
-		//return obj with x/y coords & remove from availablecoords val
-	});
+	const usedCoords = [];
+	const returnNewCoords = () => [Math.floor(Math.random() * (5 - 1 + 1) + 1), Math.floor(Math.random() * (5 - 1 + 1) + 1)];
+	const returnAvailableCoords = arr => {
+		const coords = returnNewCoords();
+
+		if (usedCoords.every(map => map[0] !== coords[0] && map[1] !== coords[1]) === false) {
+			return returnAvailableCoords(arr)
+		}
+		usedCoords.push(coords);
+		return coords
+	};
 	const compCoords = {
 		[vessels[0]]: {
 			x: ,
