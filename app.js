aacb6b9a3539fe31cc4638a832b0bcb7ec5f8a0d
Tim <timevko@gmail.com>
2016-09-29 22:21:33 -0400

    prog

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
-const apiController = '/controllers/ap/');
+const apiController = 'controllers/api';
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //API Routing
 
 //create new game
-app.put('/api/newgame/', require(`/${apiController}/createGame`));
+app.put('/api/newgame/', require(`./${apiController}/createGame`));
 
 app.delete('/api/destroygame/', (req, res, next) => {
 	//delete game
--- controllers/api/createGame.js
+++ controllers/api/createGame.js
@@ -1,46 +1,69 @@
 //create a new game
+const express = require('express');
+const app = express();
 
-const createGame = (req, res, next) => {
+const createGame = () => {
 	/***
-	*** User coords should be
-	*** an object whos keys are
-	*** vessel names, and values
-	*** are objects containing x/y
-	**** coordinates
+	*** Creates a computer gameboard with a success msg
+	*** or a failure message in case an error occurs
 	***/
-	const userCoords = req.body.userGameCoords;
-	const vessels = ['carrier', 'battleship', 'jetski', 'armed octopus', 'squirtgun'];
-	const usedCoords = [];
-	const returnNewCoords = () => [Math.floor(Math.random() * (5 - 1 + 1) + 1), Math.floor(Math.random() * (5 - 1 + 1) + 1)];
-	const returnAvailableCoords = arr => {
-		const coords = returnNewCoords();
+	try {
+		const playerGameBoard = {};
+		const usertoken = new Buffer(Date.now() + '').toString('base64');
+		const vessels = ['carrier', 'battleship', 'jetski', 'armed octopus', 'squirtgun'];
+		const usedCoords = [];
+		const returnNewCoords = () => [Math.floor(Math.random() * (5 - 1 + 1) + 1), Math.floor(Math.random() * (5 - 1 + 1) + 1)];
+		const returnAvailableCoords = arr => {
+			const coords = returnNewCoords();
 
-		if (usedCoords.every(map => map[0] !== coords[0] && map[1] !== coords[1]) === false) {
-			return returnAvailableCoords(arr)
+			if (usedCoords.every(map => map[0] !== coords[0] && map[1] !== coords[1]) === false) {
+				return returnAvailableCoords(arr)
+			}
+			usedCoords.push(coords);
+			return coords
+		};
+		const compGameBoard = {};
+
+		vessels.forEach(vessel => {
+			const coords = returnAvailableCoords(usedCoords);
+			compGameBoard[vessel.replace(' ', '_')] = {
+				x: coords[0],
+				y: coords[1],
+				vessel
+			}
+		});
+
+		if (app.locals.games === undefined) {
+			app.locals.games = {};
 		}
-		usedCoords.push(coords);
-		return coords
-	};
-	const compCoords = {
-		[vessels[0]]: {
-			x: ,
-			y:
-		},
-		[vessels[1]]: {
-			x: ,
-			y:
-		},
-		[vessels[2]]: {
-			x: ,
-			y:
-		},
-		[vessels[3]]: {
-			x: ,
-			y:
-		},
-		[vessels[4]]: {
-			x: ,
-			y:
-		},
-	};
+
+		app.locals.games[usertoken] = {
+			compGameBoard,
+			playerGameBoard,
+			moves: {
+				comp: [],
+				usr: []
+			}
+		};
+
+		return {
+			success: true,
+			usertoken,
+			compGameBoard
+		}
+
+	} catch(e) {
+
+		return {
+			success: false,
+			message: e
+		}
+	}
 };
+
+const returnNewGame = (req, res, next) => {
+	console.log(req.body);
+	res.json(createGame());
+};
+
+module.exports = returnNewGame;
